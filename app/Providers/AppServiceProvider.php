e3f4b24ee631d58dd793cf3fa891c66b2578d713
Tao Thien Nhan <taothiennhan.official@gmail.com>
2023-12-26 11:11:25 +0700

    [Service] Register CustomCuratorPlugin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Core\Plugin\CustomCuratorPlugin;
+use Awcodes\Curator\CuratorPlugin;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CuratorPlugin::class, function ($app) {
+            return new CustomCuratorPlugin();
+        });
     }
 
     /**
