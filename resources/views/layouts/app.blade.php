ae67ff87c6a37a132c62b68ea718d9d978993dc3
Tao Thien Nhan <taothiennhan.official@gmail.com>
2023-12-26 20:51:26 +0700

    [Meta Tag] Add description, keywords, author

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+    <meta name="description" content="Website thư viện hình ảnh do AI tạo ra thuộc sở hữu của AI Art, những ảnh này chia sẽ cho cộng đồng, mọi người có thể truy cập và tải ảnh mong muốn" />
+    <meta name="keywords" content="free image, image, photo, ai image, AI, AI photo, AI Art, aiart.info.vn, ảnh của ai, ai thiết kế, thiết kế ảnh bằng ai, thư viện ảnh AI, AI tạo ảnh" />
+    <meta name="author" content="Tào Thiện Nhân" />
+
     <title>{{ config('app.name') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
