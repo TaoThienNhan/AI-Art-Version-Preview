f55d8c2c730c152167953b6b580a9d336d54a222
Tao Thien Nhan <taothiennhan.official@gmail.com>
2023-12-26 11:28:37 +0700

    [Plugin] LanguageSwitch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Core\Plugin\CustomCuratorPlugin;
 use Awcodes\Curator\CuratorPlugin;
+use BezhanSalleh\FilamentLanguageSwitch\Enums\Placement;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['en','vi'])
+                ->visible(outsidePanels: true)
+                ->outsidePanelPlacement(Placement::BottomRight)
+                ->renderHook('panels::global-search.before')
+                ->circular();
+        });
     }
 }
