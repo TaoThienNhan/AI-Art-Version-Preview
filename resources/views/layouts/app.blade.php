64530911a4fa13d12e5ccc3b69cb1321b6b098f7
Tao Thien Nhan <taothiennhan.official@gmail.com>
2023-12-27 11:01:24 +0700

    [Meta] add google-site-verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8" />
-
+    <meta name="google-site-verification" content="tvMUoiOD5KGvUWLdQJvL6T-DswSm5pL9FSyJi28ehvM" />
     <meta name="application-name" content="{{ config('app.name') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
